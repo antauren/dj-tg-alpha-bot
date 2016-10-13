377c5fd440bc51535d982ae304543ae0fc6e9cf1
Eugene Lisitsky <lisitsky@gmail.com>
2016-10-13 15:04:54 +0300

    Nano fixes for flake8 tests

--- dj_tg_bot/settings.py
+++ dj_tg_bot/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+# for heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,7 +87,7 @@ DATABASES = {
 
 # Update database configuration with $DATABASE_URL.
 # Required for heroku installation
-import dj_database_url
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
