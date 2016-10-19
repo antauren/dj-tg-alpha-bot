91361a4921464fec87bf545c44240ea754166795
Eugene Lisitsky <lisitsky@gmail.com>
2016-10-19 17:44:25 +0300

    Fix - commited with conflict markers

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -3,46 +3,24 @@ Test Configuration of logger.
 """
 
 
-<<<<<<< HEAD
-import os
-=======
 import os, importlib
->>>>>>> master
 from unittest.mock import patch
 # from django.test import TestCase
 # from unittest import skip
 # we have to use tools outside of django, because when it's initialized
 # it's too late to change environment variables
-<<<<<<< HEAD
-from unittest import TestCase, skip
-
-class LoggingLevelTest(TestCase):
-
-=======
 from unittest import TestCase, skip, main
 
 class LoggingLevelTest(TestCase):
 
     levels = ('DEBUG', 'INFO', 'CRITICAL', 'WARNING',)
 
->>>>>>> master
     # @skip
     def test_loglevel_debug(self):
         """
         Checks config accepts environment variable
         DJANGO_LOG_LEVEL and sets log level according to it
         """
-<<<<<<< HEAD
-        env_var = 'DJANGO_LOG_LEVEL'
-        with patch.dict('os.environ', {env_var: 'DEBUG'}):
-            from dj_tg_bot import settings
-            print(os.environ[env_var])  # should print out 'newvalue'
-            assert env_var in os.environ  # should be True
-            print(settings.LOGGING['loggers'])
-            self.assertEqual(settings.LOGGING['loggers']['django']['level'],
-                             'DEBUG')
-        assert env_var not in os.environ  # should be True
-=======
 
         env_var = 'DJANGO_LOG_LEVEL'
         from dj_tg_bot import settings
@@ -64,4 +42,3 @@ class LoggingLevelTest(TestCase):
 
 if __name__ == '__main__':
     main()
->>>>>>> master
