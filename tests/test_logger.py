3cb5d569940a4a7cadd76f01323d40a8681f9861
Eugene Lisitsky <lisitsky@gmail.com>
2016-10-19 12:55:42 +0300

    Fix: test logger with environmental vars

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -3,7 +3,7 @@ Test Configuration of logger.
 """
 
 
-import os
+import os, importlib
 from unittest.mock import patch
 # from django.test import TestCase
 # from unittest import skip
@@ -13,6 +13,8 @@ from unittest import TestCase, skip
 
 class LoggingLevelTest(TestCase):
 
+    levels = ('DEBUG', 'INFO', 'CRITICAL', 'WARNING',)
+
     # @skip
     def test_loglevel_debug(self):
         """
@@ -20,11 +22,19 @@ class LoggingLevelTest(TestCase):
         DJANGO_LOG_LEVEL and sets log level according to it
         """
         env_var = 'DJANGO_LOG_LEVEL'
-        with patch.dict('os.environ', {env_var: 'DEBUG'}):
-            from dj_tg_bot import settings
-            print(os.environ[env_var])  # should print out 'newvalue'
-            assert env_var in os.environ  # should be True
-            print(settings.LOGGING['loggers'])
-            self.assertEqual(settings.LOGGING['loggers']['django']['level'],
-                             'DEBUG')
-        assert env_var not in os.environ  # should be True
+        for level in self.levels:
+            with patch.dict('os.environ', {env_var: level}):
+                from dj_tg_bot import settings
+                importlib.reload(settings)
+                # print(os.environ[env_var])  # should print out 'newvalue'
+                assert env_var in os.environ  # should be True
+                # print(settings.LOGGING['loggers'])
+                # django application should get logging setting DJANGO_LOG_LEVEL
+                self.assertEqual(
+                    settings.LOGGING['loggers']['django']['level'], level
+                )
+                # geogame too
+                self.assertEqual(
+                    settings.LOGGING['loggers']['geogame']['level'], level
+                )
+            assert env_var not in os.environ  # should be True
