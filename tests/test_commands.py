89f304b1e0cf9ea817a34b144505539d2aba33cf
Eugene Lisitsky <lisitsky@gmail.com>
2016-10-24 23:46:09 +0300

    Added test for command. First /start is accepted well and reply is ok

---
+++ tests/test_commands.py
@@ -0,0 +1,37 @@
+import telegram
+from telegrambot.test import testcases
+try:
+    from unittest import mock
+except ImportError:
+    import mock  # noqa
+
+
+class TestSimpleCommands(testcases.BaseTestBot):
+    start = {'in': '/start',
+             'out': {'parse_mode': 'Markdown',
+                     'reply_markup': '',
+                     'text': "Welcome"}
+             }
+
+    def setUp(self):
+        # Bot upon saving tries to install webhook in real telegram
+        # But it just breaks tests because of illegal data
+        # So it's better to mock a Bot.setWebhook method
+        self._original_setWebhook = telegram.bot.Bot.setWebhook
+        self.patcher = mock.patch('telegram.bot.Bot.setWebhook')
+        self._patched_setWebhook = self.patcher.start()
+        super().setUp()
+
+    def tearDown(self):
+        # Stop patcher and restore method as it was before
+        self.patcher.stop()
+
+    def test_patched_successfully(self):
+        assert telegram.bot.Bot.setWebhook is not self._original_setWebhook
+        assert telegram.bot.Bot.setWebhook is self._patched_setWebhook
+
+    def test_start(self,):
+        """
+        Test bot accept normally command /start and replies as it should.
+        """
+        self._test_message_ok(self.start)
