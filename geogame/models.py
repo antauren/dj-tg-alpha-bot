cd86ee15186a3b51454b31875156a94c431d1cc6
lisitsky <lisitsky@gmail.com>
2016-10-28 12:11:41 +0300

    Post compile script to run migrations on heroku (#12)
    * Post compile script to run migrations on heroku
    
    * Add migrations
    
    * flake8 ignore migrations

--- geogame/models.py
+++ geogame/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
 
 # Create your models here.
+
+
+class Amodel(models.Model):
+    """
+    This is a model just to test auto migrations on Heroku deploy.
+    """
+    name = models.CharField('Name', max_length=100)
