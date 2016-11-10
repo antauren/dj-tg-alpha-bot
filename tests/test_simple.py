7e09bb0ae05247e98827f09413e679bcb89fc535
Eugene Lisitsky <lisitsky@gmail.com>
2016-11-10 15:06:48 +0300

    Added test for Scenario.__str__

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,7 +1,14 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
-
+from django.test import TestCase
+from geogame.models import Scenario
 
 def test_success():
     assert True
+
+
+class TestScenario(TestCase):
+
+    def test_scenario_str_method(self):
+        name = 'Some scenario name'
+        scenario = Scenario(name=name)
+        self.assertEqual(str(scenario), name)
+        assert str(scenario), name
