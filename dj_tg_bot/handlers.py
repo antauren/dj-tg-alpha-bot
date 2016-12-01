1b06b3912edcd34e18bd1e012cfb4f922dd12ab0
Eugene Lisitsky <lisitsky@gmail.com>
2016-12-01 11:32:30 +0300

    Added new handler and template for start1 command

--- dj_tg_bot/handlers.py
+++ dj_tg_bot/handlers.py
@@ -1,8 +1,10 @@
 from telegrambot.handlers import command, unknown_command
-from geogame.bot_views import StartView, ScenariosListCommandView, UnknownView
+from geogame.bot_views import StartView, StartView1, \
+    ScenariosListCommandView, UnknownView
 
 urlpatterns = [
     command('start', StartView.as_command_view()),
+    command('start1', StartView1.as_command_view()),
     command('list', ScenariosListCommandView.as_command_view()),
     unknown_command(UnknownView.as_command_view()),
 ]
